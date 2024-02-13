7ed03adebbb5c7bbecf608b058cfbb2a043dc35e
Universal99OS <pandukapiushanpullaperuma4321@gmail.com>
2024-02-14 01:10:50 +0530

    Initial Commit

---
+++ src/app/pages/view-all-books/view-all-books.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-view-all-books',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-all-books.component.html',
+  styleUrl: './view-all-books.component.css'
+})
+export class ViewAllBooksComponent implements OnInit {
+  
+  private http;
+  public allBooks:any={};
+
+  constructor(private httpClient:HttpClient){
+    this.http=httpClient;
+
+  }
+  ngOnInit(): void {
+    this.loadAllBooks();
+  }
+  loadAllBooks() {
+    this.http.get("http://localhost:8080/book/get").subscribe((data)=>{
+      console.log(data);
+    })
+  }
+
+}
+
+
